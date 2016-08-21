5f605907f1a9511d1714f372b546c01c6de9f473
“chase93” <chasesmilano@gmail.com>
2016-08-21 17:57:57 +0100

    yes

--- features/step_definitions/test.rb
+++ features/step_definitions/test.rb
@@ -5,19 +5,21 @@ Given(/^I am on the Acumen Contact Us page$/) do
 end
 
 When(/^I navigate to the Contact Us page$/) do
-  puts 'step 2'
+  puts 'Go to contact us page'
 end
 
 Then(/^I should see an accurate post code "(TW9 1HY)"$/) do |arg1|
-  puts 'step 3'
+  puts 'I am now on the page'
 end
 When(/^I submit just an email address and some message text$/) do
   fill_in('your-email', :with => 'cmilano@bookingbug.com')
   fill_in('your-message', :with => 'blah blah blah')
+puts 'I fill in some stuff'
 end
 
 When(/^all other fields are blank$/) do
    page.find(:xpath, '//*[@id="wpcf7-f682-p495-o1"]/form/p[8]/input').click
+puts 'I recieve all the errors'
 end
 
 Then(/^I should see errors about other required fields$/) do
