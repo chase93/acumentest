84fbb07299e3f05a7288b223eb28e3c33ad3a134
chase93 <chasesmilano@gmail.com>
2016-08-22 17:04:50 +0100

    Update test.rb

--- features/step_definitions/test.rb
+++ features/step_definitions/test.rb
@@ -8,22 +8,22 @@ When(/^I navigate to the Contact Us page$/) do
 end
 
 Then(/^I should see an accurate post code "(TW9 1HY)"$/) do |arg1|
-  expect(page).have_content("TW9 1HY")
+     expect(page).to have_content("TW9 1HY", wait:10)
   puts 'I am now on the page'
 end
 When(/^I submit just an email address and some message text$/) do
   fill_in('your-email', :with => 'cmilano@bookingbug.com')
   fill_in('your-message', :with => 'blah blah blah')
-  expect(page).to have_content("cmilano@bookingbug.com")
+     expect(page).to have_content("cmilano@bookingbug.com", wait:15)
 puts 'I fill in some stuff'
 end
 
 When(/^all other fields are blank$/) do
    page.find(:xpath, '//*[@id="wpcf7-f682-p495-o1"]/form/p[8]/input').click
-   expect(page).to have_content("Validation errors occurred", wait:5)
 puts 'I recieve all the errors'
 end
 
 Then(/^I should see errors about other required fields$/) do
+ expect(page).to have_content("Vaildation errors occured", wait:10)
   puts 'I win'
 end
